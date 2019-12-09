198b5debbe5e071e90570cede8fb51b28b078eee
yospig <hotaquariusplus@gmail.com>
2019-12-09 12:51:24 +0900

    Add: get a datedoc function
    - Mod: rename Save function
    - Add: get a datedoc function
    - Add: query param {user}

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -31,12 +31,9 @@ export const makeUppercase = functions.database.ref('/messages/{pushId}/original
     return snapshot.ref.parent.child('uppercase').set(uppercase);
 });
 
-// SaveDateTime
-export const SaveDateTime = functions.https.onRequest(async (req, res) => {
+// CheckInTime
+export const CheckInTime = functions.https.onRequest(async (req, res) => {
     const now: any = admin.firestore.FieldValue.serverTimestamp();
-    // query param
-//    const intimeString = req.query.in;
-
     // FIX: it is UTC... need JST
     //      year, month, day, hour are wrong.
     const setDate: Date = new Date();
@@ -50,32 +47,99 @@ export const SaveDateTime = functions.https.onRequest(async (req, res) => {
         day: setDate.getDate(),
         timestamp: now
     });
-    const dateUnderUserDocRef = dateDocRef.collection('user').doc('yospig');
+    // TODO: Should be separated
+    const user: string = (req.query.user !== null) ? req.query.user : 'yospig';
+    const dateUnderUserDocRef = dateDocRef.collection('user').doc(user);
+    // merge指定なしの場合とupdateはintimeで値が入っていてもdocumentすべて上書きになる
     await dateUnderUserDocRef.set({
-        in_hour: setDate.getHours(),
-        in_min: setDate.getMinutes(),
+        in_time_str: setDate.getHours().toString() + ':' + setDate.getMinutes().toString(),
+        in_time: {
+            in_hour: setDate.getHours(),
+            in_min: setDate.getMinutes()
+        },
         timestamp: now
     }).then(() => {
-        res.send("Add Successful: " + setDate.toDateString());
+        console.log("user: ", user, " , intime: ", setDate.toDateString());
+        res.send("Add InTime Successful: " + setDate.toDateString());
     });
 });
 
+// CheckOutTime
+export const CheckOutTime = functions.https.onRequest(async (req, res) => {
+    const now: any = admin.firestore.FieldValue.serverTimestamp();
+    const setDate: Date = new Date();
+    const dateDoc = {
+        year: setDate.getFullYear(),
+        month: setDate.getMonth() + 1,
+        day: setDate.getDate(),
+        timestamp: now
+    }
+    const outTime = {
+        out_time_str: setDate.getHours().toString() + ':' + setDate.getMinutes().toString(),
+        out_time: {
+            out_hour: setDate.getHours(),
+            out_min: setDate.getMinutes()
+        },
+        timestamp: now
+    }
+    const dateDocName: string = dateDoc.year.toString() + dateDoc.month.toString() + dateDoc.day.toString()
+    const dateDocRef = fs.collection('attendance').doc(dateDocName);
+    await dateDocRef.set(
+        dateDoc,{merge:true}
+    );
+    // TODO: Should be separated
+    const user: string = (req.query.user !== null) ? req.query.user : 'yospig';
+    const dateUnderUserDocRef = dateDocRef.collection('user').doc(user);
+    // merge指定しているのでinTimeの値は上書きされない
+    await dateUnderUserDocRef.set(
+        outTime,{merge:true}
+    ).then(() => {
+        console.log("user: ", user, " , intime: ", setDate.toDateString());
+        res.send("Add OutTime Successful: " + setDate.toDateString());
+    });
+});
+
+export const fetchDateDoc = functions.https.onRequest(async (req, res) => {
+    // query param
+    const day: string = req.query.day;
+    console.log(day)
+    await admin.firestore().collection('attendance').doc(day).get(
+    ).then(resDoc => {
+        if (resDoc.exists) {
+            res.send(resDoc.data());
+
+        } else {
+            res.send("document is not exist");
+        }
+    });
+    // const user: string = req.query.user;
+    // const year: string = day.substr(0,4);
+    // await admin.firestore().collection('attendance').orderBy('id').startAt(year).endAt(year+'\uf8ff').get()
+    // .then(function(querySnapshot) {
+    //     querySnapshot.forEach(function(doc) {
+    //         console.log(doc.id, " => ", doc.data());
+    //         res.send(JSON.stringify(doc));
+    //     });
+    // })
+    // .catch(function(error) {
+    //     console.log("Error getting documents: ", error);
+    // });
+});
+
+// fizzbuzz is test function
 export const fizzbuzz = functions.https.onRequest((request, response) => {
-    // without type
-    let fb = ""
+    const fb: Array<string | number> = [];
     for (let i = 1; i <= 100; i++) {
         if (i % 3 === 0 && i % 5 === 0) {
-            fb += "FizzBuzz";
+            fb.push("FizzBuzz")
         } else if (i % 3 === 0) {
-            fb += "Fizz";
+            fb.push("Fizz")
         } else if (i % 5 === 0) {
-            fb += "Buzz";
+            fb.push("Buzz")
         } else {
-            fb += i
+            fb.push(i)
         }
-        fb += " ";
     }
     response.send(fb);
-    // use type
 });
 
